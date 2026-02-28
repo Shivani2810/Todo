e60ba2fa7e56e633af6548ae1e4096c161cfc565
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-02-28 18:47:01 +0530

    Structure Files

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose= require ("mongoose");
+
+require("dotenv").config()
+
+const dbConnect=()=>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlPareser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>console.log("Database Connection Successful"))
+    .catch((error)=>{
+        console.log("Error in Database Connection");
+        process.exit(1);
+    })
+
+    module.exports=dbConnect;
+}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const express= require ("express");
+const app=express();
+
+app.listen(3000,()=>{
+    console.log("App is running at port 8000");
+})
+
